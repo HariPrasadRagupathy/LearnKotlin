eb4445b8be1615c5c9faf3595c972e67bf1e2602
hariprasad <hariprasadragupathy@gmail.com>
2024-07-17 11:50:55 +0530

    implement annotation span style

--- app/src/main/java/com/hp/learnkotlin/ui/compose/data/AnnotationString.kt
+++ app/src/main/java/com/hp/learnkotlin/ui/compose/data/AnnotationString.kt
@@ -12,7 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.ExperimentalTextApi
@@ -21,11 +24,15 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.style.Hyphens
 import androidx.compose.ui.text.style.LineBreak
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextGeometricTransform
 import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -84,23 +91,24 @@ fun AnnotatedStringDemo() {
 @Composable
 fun AnnotatedStringParagraphStyle() {
 
-
-
     val paragraphStyle = ParagraphStyle(
         textAlign = TextAlign.Left,
-        lineHeight = 50.sp,
-        lineHeightStyle = LineHeightStyle(alignment = LineHeightStyle.Alignment.Center,trim = LineHeightStyle.Trim.LastLineBottom),
+        lineHeight = 20.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Center,
+            trim = LineHeightStyle.Trim.LastLineBottom
+        ),
         textIndent = TextIndent(firstLine = 24.sp, restLine = 0.sp),
-
+        lineBreak = LineBreak.Paragraph,
+        hyphens = Hyphens.Auto,
+        textMotion = TextMotion.Animated
     )
-
-
 
     val styledParagraph = buildAnnotatedString {
         withStyle(paragraphStyle) {
             append("This is a paragraph with a line height of 24sp.\n")
             append("Notice how the extra space is distributed by default.\n")
-            append("Line height styling is not applied here.")
+            append("Line height styling is not applied here  in the area of the screen.")
         }
     }
     Column(
@@ -118,8 +126,52 @@ fun AnnotatedStringParagraphStyle() {
 
 @Preview
 @Composable
-fun AnnotatedStringSpanStyle(){
-    val spanStyle = SpanStyle(
-
+fun AnnotatedStringSpanStyle() {
+    val styledSpan = SpanStyle(
+        color = Color.Red,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
     )
+
+    val styledSuperScript = styledSpan.copy(
+        baselineShift = BaselineShift(1f),
+        fontSize = 12.sp
+    )
+
+    val styledTexedGeometery = styledSpan.copy(
+        textGeometricTransform = TextGeometricTransform(skewX = -0.5f)
+    )
+
+    val shadowStyle = styledSpan.copy(shadow = Shadow(color = Color.Gray, blurRadius = 10f, offset = Offset(2f, 2f)))
+
+
+    val annotatedString = buildAnnotatedString {
+        withStyle(style = styledSpan) {
+            append("Hello")
+        }
+        withStyle(style = styledSuperScript) {
+            append("rs")
+        }
+        withStyle(style = styledTexedGeometery) {
+            append("Captain")
+        }
+        append("\n\n")
+        withStyle(style = shadowStyle) {
+            append("Shadow")
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Top check for triming the above Line")
+        Text(text = annotatedString)
+    }
+    /*   withStyle(style = SpanStyle(brush = Brush.horizontalGradient(listOf(Color.Red, Color.Cyan,
+           Color.Green), startX = 10f), fontSize = 24.sp)){
+           append("Color full text")
+       }*/
+
 }
